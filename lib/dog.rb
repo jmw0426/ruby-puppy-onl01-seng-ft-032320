1de687229d36284e57d6bb29118eb685ebea1aef
Joshua Williams <jmw0426@hotmail.com>
2020-04-05 21:07:49 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -7,23 +7,24 @@ attr_reader :save
   def initialize(name)
    @name = name
    @save = save
-end 
+  end 
 
-def self.all
-@@all
-end
+  def self.all
+  @@all
+  end
 
-def self.print_all
-@@all.each do |dog|
-  puts dog.name
-end
-end
+  def self.print_all
+  @@all.each do |dog|
+    puts dog.name
+    end
+  end
 
-def save
-  @@all << self
-end
+  def save
+   @@all << self
+  end
 
-def self.clear_all
-  @@all.clear
-end
+  def self.clear_all
+   @@all.clear
+  end
+
 end 
